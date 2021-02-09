36cd109eb8aad6d8ea7e9641ca7bcc7728558113
“Artyvas” <artemvasily@gmail.com>
2021-02-08 17:39:49 -0800

    Demonstrates git functionality to Ivan

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -1,2 +1,5 @@
 class Api::MapsController < ApplicationController
+  def Ivan
+    puts "Hey Ivan, these are some changes that will be reflected by git. You will see three green lines on the left, and this will indicate that these lines were added. If you upload via UI, you don't get the changes records like you do from a linked folder"
+  end
 end
