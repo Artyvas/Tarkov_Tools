b1e2c1628085f2c79d4e297f7ced3815c250c530
“Artyvas” <artemvasily@gmail.com>
2021-03-10 19:42:40 -0800

    Tweaks Key model in order to enable creation of test data. Ready for test data now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-SearchableObject.create({ location_code: "C5", image_id: "N/A", object_name: "Wooden Crate", description: "Hit this cluster of crates if you're spawning/extracting crossroads", needskey: "FALSE", key_id: "N/A" })
-SearchableObject.create({ location_code: "B5", image_id: "N/A", object_name: "Wooden Crate", description: "Hit this cluster of crates if you're spawning/extracting crossroads", needskey: "FALSE", key_id: "N/A" })
-SearchableObject.create({ location_code: "H4", image_id: "N/A", object_name: "Weapon Box/Rack", description: "4x4 Weapon box", needskey: "FALSE", key_id: "N/A" })
-SearchableObject.create({ location_code: "H4", image_id: "N/A", object_name: "Jacket", description: "Blue lootable jacket in corner of storage container", needskey: "FALSE", key_id: "N/A" })
-SearchableObject.create({ location_code: "H4", image_id: "N/A", object_name: "Loose Loot", description: "Trunk commonly holds AK and loose bullet - needs car key to access", needskey: "TRUE", key_id: "N/A" })
-SearchableObject.create({ location_code: "H6", image_id: "N/A", object_name: "Sports Bag", description: "It's a sports bag - just that and nothing else", needskey: "FALSE", key_id: "N/A" })
+SearchableObject.create({ location_id: "37", image_id: "N/A", object_name: "Wooden Crate", description: "Hit this cluster of crates if you're spawning/extracting crossroads", needskey: "FALSE", key_id: "N/A" })
+SearchableObject.create({ location_id: "21", image_id: "N/A", object_name: "Wooden Crate", description: "Hit this cluster of crates if you're spawning/extracting crossroads", needskey: "FALSE", key_id: "N/A" })
+SearchableObject.create({ location_id: "116", image_id: "N/A", object_name: "Weapon Box/Rack", description: "4x4 Weapon box", needskey: "FALSE", key_id: "N/A" })
+SearchableObject.create({ location_id: "116", image_id: "N/A", object_name: "Jacket", description: "Blue lootable jacket in corner of storage container", needskey: "FALSE", key_id: "N/A" })
+SearchableObject.create({ location_id: "116", image_id: "N/A", object_name: "Loose Loot", description: "Trunk commonly holds AK and loose bullet - needs car key to access", needskey: "TRUE", key_id: "N/A" })
+SearchableObject.create({ location_id: "118", image_id: "N/A", object_name: "Sports Bag", description: "It's a sports bag - just that and nothing else", needskey: "FALSE", key_id: "N/A" })
+
+Key.create!({ location_id: 1, name: "N/A", market_price: 0, trader_price: 0, trader: "N/A", searchable_object_id: 1, loot_room_id: 1, image_url: "N/A" })
